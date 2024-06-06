929dd203aef5f90b6459b669971ce08cdbfa7d76
Sarah Babiker <sarahbabiker4@gmail.com>
2024-06-06 11:49:52 -0500

    Initial Commit

---
+++ STEMstars/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  STEMstars
+//
+//  Created by Scholar on 6/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack {
+         
+            Color.yellow.opacity(0.3)
+                .ignoresSafeArea()
+            VStack(alignment: .center, spacing: 15.0){
+                Text("Women In STEM")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.604, green: 0.049, blue: 0.054))
+                    .multilineTextAlignment(.center)
+                Image("hannaschmitz")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                
+                HStack(alignment: .center, spacing: 30.0) {
+                    Text("Hanna Schmitz")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    Text("Racing Engineer")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.913, brightness: 0.605))
+                        .padding()
+                    
+                }
+
+                
+                Text("Hannah Schmitz is a British engineer, currently working for Austrian Formula One team Red Bull Racing as Principal Strategy Engineer.")
+                    .font(.subheadline)
+                    .padding()
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius:15)
+            .padding()
+            
+        }
+            
+        
+        }
+        
+        
+    }
+
+
+#Preview {
+    ContentView()
+}
---
+++ STEMstars/STEMstarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMstarsApp.swift
+//  STEMstars
+//
+//  Created by Scholar on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct STEMstarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
